d5e4f7f567dfd5135406d5ee9480b1347fe1f137
Marc D Wagner <marc.wagner@yahoo.com>
2022-11-01 09:16:37 -0700

    challenge 4 demo

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -64,6 +64,11 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 
 const divisibleByFiveTwoToThePower = (input) => {
   // Solution code here...
+  return input.map(nestedArr => {
+    return nestedArr.filter(element => element % 5 === 0 && typeof element === 'number')
+      .map(filteredElement => Math.pow(2, filteredElement));
+
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
