d36335e4589d7d52f66bc3f327ba7800f24fdf09
Marc D Wagner <marc.wagner@yahoo.com>
2022-12-26 18:57:35 -0700

    created readme and started with classes

---
+++ python/docs/linked_list/main.py
@@ -0,0 +1,11 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+class Node:
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+
