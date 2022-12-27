678d7620ce332c23a8447bee70cf451d2c9d6428
Marc D Wagner <marc.wagner@yahoo.com>
2022-12-27 08:41:52 -0700

    added classes and nodes

--- python/docs/linked_list/main.py
+++ python/docs/linked_list/main.py
@@ -4,8 +4,17 @@ class LinkedList:
 
 
 class Node:
-    def __init__(self, value, next):
+    def __init__(self, value):
         self.value = value
-        self.next = next
+        self.next = None
 
 
+first_node = Node("A")
+second_node = Node("B")
+third_node = Node("C")
+
+link_list = LinkedList()
+
+link_list.head = first_node
+first_node.next = second_node
+second_node.next = third_node
