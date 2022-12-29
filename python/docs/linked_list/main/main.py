82c19b0ac31fe6345e4bd6845c8dedcd902fcef5
Marc D Wagner <marc.wagner@yahoo.com>
2022-12-28 20:23:35 -0700

    finally made some headway, set up insert, includes and str

--- python/docs/linked_list/main/main.py
+++ python/docs/linked_list/main/main.py
@@ -1,23 +1,68 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value=None, next=None):
         self.value = value
-        self.next = None
-
-    def __str__(self):
-        return f"{self.value}"
+        self.next = next
 
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
+    def __init__(self, head=None):
+        self.head = head
+        # if nodes is not None:
+        #     node = Node(value=nodes.pop())
+        #     self.head = node
+        #     for element in nodes:
+        #         node.next = Node(value=element)
+        #         node = node.next
 
 
-first_node = Node("a")
-second_node = Node("b")
-third_node = Node("c")
+    def insert(self, new_value):
+        new_node = Node(new_value)
+        new_node.next = self.head
+        self.head = new_node
 
-link_list = LinkedList()
+    def includes(self, value):
+        current = self.head
+        while current != None:
+            return True
+        current = current.next
+        return False
 
-link_list.head = first_node
-first_node.next = second_node
-second_node.next = third_node
+
+    def __str__(self):
+        return f" {self.value} -> "
+        # node = self.head
+        # nodes = []
+        # while node is not None:
+        #     nodes.append(node.value)
+        #     node = node.next
+        # nodes.append("None")
+        # return " -> ".join(nodes)
+
+
+    # def print_ll(self):
+    #     current = self.head
+
+        # while current:
+        #     print(current.value)
+        #     current = current.next
+
+    # def visualize(self):
+    #     node = self.head
+    #     nodes = []
+    #     while node is not None:
+    #         nodes.append(node.value)
+    #         node = node.next
+    #
+    #     nodes.append("None")
+    #     print(" -> ".join(nodes))
+
+
+
+
+# if __name__ == "__main__":
+#     Node("apple")
+    # insert("banana")
+# ll_1 = LinkedList("apple")
+# ll_1.head.next = Node("banana")
+# ll_1.head.next.next = Node("cucumber")
+# ll_1.visualize()
